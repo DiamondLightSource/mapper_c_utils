6798e3c5943d5062a740dd0e5ff10364423fa64f
pmousley <philipmousley@hotmail.com>
2025-11-12 15:30:01 +0000

    update setup to use setuptools and sysconfig

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from distutils.core import setup, Extension
 import sysconfig
+from setuptools import setup, Extension
+import numpy
+
 
 EXTRA_COMPILE_ARGS = sysconfig.get_config_var('CFLAGS').split()
 EXTRA_COMPILE_ARGS += ["-std=c99"]
 
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    from numpy.distutils.misc_util import get_info
-
-    # Necessary for the half-float d-type.
-    info = get_info('npymath')
-
-    config = Configuration('',
-                           parent_package,
-                           top_path)
-    config.add_extension('mapper_c_utils',
-                         ['src/mapper_c_utils/mapper_c_utils.c'],
-                         extra_info=info,
-                         language='c99',
-                         extra_compile_args=EXTRA_COMPILE_ARGS)
-
-    return config
+ext_modules = [
+    Extension(
+        "mapper_c_utils",
+        sources=["src/mapper_c_utils/mapper_c_utils.c"],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=EXTRA_COMPILE_ARGS,
+        language="c99"
+    )
+]
 
 
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(name='mapper_c_utils', version='1.0.0', configuration=configuration)
+
+if __name__ == "__main__":  
+    setup(
+        name="mapper_c_utils",
+        version="1.1.0",
+        ext_modules=ext_modules,
+    )
+
