85e67c03a689379ac7a120bc26bfb156e28d5c5a
P-Mousley <philipmousley@hotmail.com>
2026-07-08 17:31:17 +0100

    switch back to numpy.disutils

--- setup.py
+++ setup.py
@@ -2,20 +2,26 @@
 latest version created: parsing_detector in mapper_c_utils/
 """
 
-import numpy
-from setuptools import Extension, setup
+import sysconfig
 
-ext_modules = [
-    Extension(
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration, get_info
+
+EXTRA_COMPILE_ARGS = sysconfig.get_config_var("CFLAGS").split() + ["-std=c99"]
+
+
+def configuration(parent_package="", top_path=None):
+    info = get_info("npymath")
+    config = Configuration("", parent_package, top_path)
+    config.add_extension(
         "mapper_c_utils",
-        sources=["src/mapper_c_utils/mapper_c_utils.c"],
-        include_dirs=[numpy.get_include()],
-        extra_compile_args=["-std=c99"],
+        ["src/mapper_c_utils/mapper_c_utils.c"],
+        extra_info=info,
+        language="c99",
+        extra_compile_args=EXTRA_COMPILE_ARGS,
     )
-]
+    return config
 
-setup(
-    name="mapper_c_utils",
-    version="1.1.0",
-    ext_modules=ext_modules,
-)
+
+if __name__ == "__main__":
+    setup(name="mapper_c_utils", version="1.1.0", configuration=configuration)
